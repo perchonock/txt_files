184d5ea9150563ff201f5028dec6a30371e38ac0
perchonok <me_on@mail.ru>
2014-10-29 23:28:36 +0400

    Sorting by frequency
    sort dictionary by value

--- counting_frequency.py
+++ counting_frequency.py
@@ -2,13 +2,17 @@ __author__ = 'YKolotolova'
 
 from splitting_text_into_ngramms import *
 
-# file = open("test_en.txt", 'r')
-# file_of_words = open("list_of_words.txt", 'w')
-# text = file.read()
-#
-# splitter = TextSplitter()
-# list_of_words = splitter.split_into_words(text)
+file = open("test_en.txt", 'r')
+#file_of_words = open("list_of_words.txt", 'w')
+text = file.read()
 
+splitter = TextSplitter()
+list_of_words = splitter.split_into_words(text)
+
+def get_item(a):
+    def get_i(pair):
+        return pair[a]
+    return get_i
 
 class FrequencyCounter:
     def count_words(self, list_of_words):
@@ -19,14 +23,44 @@ class FrequencyCounter:
                dict_of_words[word] +=1
             else:
                 dict_of_words[word] = 1
-        words = '' #убрать, когда буду писать в файл, также убрать += в цикле ниже и return words
-        for word in sorted(dict_of_words):
-            words += str(word) + " " + str(dict_of_words[word]) + "\n"
-#             file_of_words.write(words)
+        return dict_of_words
+
+    def sort_by_frequency(self, dict_of_words):
+        words = ''
+        for key_value in sorted(dict_of_words.items(), key = get_item(1), reverse = True):
+            words += key_value[0] + " " + str(key_value[1]) + "\n"
         return words
 
-# word_counter = FrequencyCounter()
-# print(word_counter.count_words(list_of_words))
 
-# file.close()
-# file_of_words.close()
+word_counter = FrequencyCounter()
+dict = word_counter.count_words(list_of_words)
+#print(word_counter.sort_by_frequency(dict))
+
+file.close()
+#file_of_words.close()
+
+
+def foreach(collection, function):
+    for a in collection:
+        function(a)
+
+lst = [1,2,4,5,6]
+
+def printSquare(value):
+    print(value*value)
+
+
+#foreach(lst, printSquare)
+
+def reduce(collection, function, zeroElement):
+    result = zeroElement
+    for a in collection:
+        result = function(a, result)
+    return result
+
+def sum(a, b):
+    return a + b
+
+print(reduce(lst, lambda a,b:  a + b, 0))
+
+print(reduce(lst, lambda a,b:  a * b, 1))
--- test_FrequencyCounter.py
+++ test_FrequencyCounter.py
@@ -6,12 +6,13 @@ __author__ = 'YKolotolova'
 
 class TestFrequencyCounter(TestCase):
     def test_count_words1(self):
-        lst_of_words = ['table', 'chair', 'table', 'bla', 'new', 'table']
+        lst_of_words = ['table', 'chair', 'table', 'bla', 'new', 'table', 'chair', 'new', 'new', 'table']
         word_counter = FrequencyCounter()
         words = word_counter.count_words(lst_of_words)
-        words2 = 'bla 1\nchair 1\nnew 1\ntable 3\n'
+        words2 = 'table 4\nnew 3\nchair 2\nbla 1\n'
         self.assertTrue(words == words2, words)
-
+#написать тест, который проверяет только функцию подсчета слов. Например, просерить, что размер возвращаемого словаря правильный,
+    #содержит все слова, слова с правильным значением частоты
     def test_count_words2(self):
         lst_of_words = []
         word_counter = FrequencyCounter()
